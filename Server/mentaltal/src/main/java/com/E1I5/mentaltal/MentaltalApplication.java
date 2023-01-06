2ba776fb0e084844263d2c9c52becf32c96c063d
Sangwonee <111265742+Sangwonee@users.noreply.github.com>
2023-01-06 15:54:58 +0900

    Initial commit

---
+++ Server/mentaltal/src/main/java/com/E1I5/mentaltal/MentaltalApplication.java
@@ -0,0 +1,13 @@
+package com.E1I5.mentaltal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentaltalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentaltalApplication.class, args);
+	}
+
+}
