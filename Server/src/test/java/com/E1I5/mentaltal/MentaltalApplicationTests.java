3854a1cb24006d0facc21264ddcabd5f8b016bdb
Sangwonee <111265742+Sangwonee@users.noreply.github.com>
2023-01-07 01:26:12 +0900

    Initial Setting

---
+++ Server/src/test/java/com/E1I5/mentaltal/MentaltalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E1I5.mentaltal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentaltalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
