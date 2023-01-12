818ec3158cd7a15407315bbfd6dc1f6cecb33b9d
jinhuiju <jinhuizu@gmail.com>
2023-01-12 14:15:12 +0900

    feat : Comment 기능 구현

---
+++ Server/src/main/java/com/E1I5/mentaltal/comment/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.E1I5.mentaltal.comment.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "COMMENTS")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false, columnDefinition = "Text")
+    private String content;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime modifiedAt;
+
+    @Column
+    private int score;
+
+    private long boardId;
+
+    private long memberId;
+
+}
