99d5db69b5b3331ed5513c4a746300d7a2f99c43
Kim-Jihyun1 <jh129047@gmail.com>
2023-01-20 15:04:03 +0900

    [feat] CORS 설정

---
+++ Server/src/main/java/com/e1i5/mentaltal/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.e1i5.mentaltal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// CORS
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true) // 쿠키 요청 허용 (true 설정 시 보안상 이슈 발생 가능)
+                .allowedOrigins("http://localhost:8080");   // 공유를 허락할 origin (* : 모든 origin)
+    }
+}
