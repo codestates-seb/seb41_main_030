c2329cfa78e5b10ccd4155ebe76308f7d942aafe
xxyeon129 <xxyeon129@gmail.com>
2023-01-24 15:07:04 +0900

    [feat] 마이페이지 - 작성한 답변의 게시글 제목 서버 데이터에서 불러오기

--- client/src/components/mypage/MyPageAnswer.js
+++ client/src/components/mypage/MyPageAnswer.js
@@ -3,16 +3,45 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const MyPageAnswer = ({ userData }) => {
-    const [answerListData, setAnswerListData] = useState(undefined);
+    const [answerListData, setAnswerListData] = useState(undefined); // 전체 답변
+    const [postListData, setPostListData] = useState(undefined); // 전체 게시글
     const url = `http://ec2-3-36-53-155.ap-northeast-2.compute.amazonaws.com:8080`;
 
     useEffect(() => {
-        axios.get(`${url}/comments/all`).then((res) => {
-            setAnswerListData(res.data);
-        });
+        // 답변
+        axios
+            .get(`${url}/comments/all`)
+            .then((res) => {
+                setAnswerListData(res.data);
+            })
+            .catch((error) => console.log(error));
+
+        // 전체 게시글
+        axios
+            .get(`${url}/boards/all`)
+            .then((res) => {
+                setPostListData(res.data);
+            })
+            .catch((error) => console.log(error.response));
     }, []);
 
+    // 작성한 답변 데이터
     const userAnswerData = answerListData && userData && answerListData.filter((answerData) => answerData.nickName === userData.nickName);
+
+    // 전체 게시글 중 답변을 단 게시글
+    const result = postListData && userAnswerData && postListData.filter(({ boardId: id1 }) => userAnswerData.some(({ boardId: id2 }) => id1 === id2));
+
+    // 작성한 답변 데이터(userAnswerData)에 답변 대상 게시글의 title 추가
+    const addTitle =
+        postListData &&
+        userAnswerData &&
+        userAnswerData.map((answer) => {
+            postListData.forEach((post) => {
+                if (post.boardId === answer.boardId) {
+                    answer["title"] = post.title;
+                }
+            });
+        });
 
     return (
         <>
