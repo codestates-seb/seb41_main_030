9ed8d1ba9ad939154bfc9dc77fed12a84a0244b7
jinhuiju <jinhuizu@gmail.com>
2023-01-26 12:09:03 +0900

    Update MentaltalApplication.java - TimeZone=Asia/Seoul

--- Server/src/main/java/com/e1i5/mentaltal/MentaltalApplication.java
+++ Server/src/main/java/com/e1i5/mentaltal/MentaltalApplication.java
@@ -2,9 +2,19 @@ package com.e1i5.mentaltal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MentaltalApplication {
+
+	@PostConstruct
+	void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MentaltalApplication.class, args);
