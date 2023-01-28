ffc81c1d72f5d722f86a9c406e9be8ea099cfe22
jinhuiju <jinhuizu@gmail.com>
2023-01-29 04:13:34 +0900

    .

--- Server/src/main/java/com/e1i5/mentaltal/domain/board/service/BoardService.java
+++ Server/src/main/java/com/e1i5/mentaltal/domain/board/service/BoardService.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
--- Server/src/main/java/com/e1i5/mentaltal/domain/comment/service/CommentService.java
+++ Server/src/main/java/com/e1i5/mentaltal/domain/comment/service/CommentService.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
