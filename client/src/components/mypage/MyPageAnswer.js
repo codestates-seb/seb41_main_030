991b2b538a7c8f575b6b89a0983af10ea53caae6
xxyeon129 <xxyeon129@gmail.com>
2023-02-02 10:26:10 +0900

    [misc] 마이페이지 url  수정

--- client/src/components/mypage/MyPageAnswer.js
+++ client/src/components/mypage/MyPageAnswer.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const MyPageAnswer = ({ userData }) => {
     const [answerListData, setAnswerListData] = useState(undefined); // 전체 답변
     const [postListData, setPostListData] = useState(undefined); // 전체 게시글
-    const url = "http://ec2-43-201-14-234.ap-northeast-2.compute.amazonaws.com:8080";
+    const url = process.env.REACT_APP_SERVER_URL;
     const navigate = useNavigate();
 
     useEffect(() => {
